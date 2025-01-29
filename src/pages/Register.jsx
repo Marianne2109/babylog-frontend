ed7a84afa9dcb0c9dc4986408687829edfd42879
Marianne Veyl Neumann <15081@coderacademy.edu.au>
2025-01-30 00:53:00 +1100

    Update API path

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,7 +28,7 @@ const RegisterPage = () => {
     setSuccess(null);
 
     try{
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
         method: 'POST', 
         headers: {
           'Content-Type': 'application/json',
